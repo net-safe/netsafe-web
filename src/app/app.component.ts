601ce2dcdfb1dbb0d4914a567c36795e0d4b29cc
Jeremías <ramirezjeremias98@gmail.com>
2017-11-24 18:19:27 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../assets/styles/_netsafe.scss']
 })
 export class AppComponent {
   title = 'app';
